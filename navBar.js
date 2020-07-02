aa9f5184297fbc08a433be6d7c30f31815879bed
Felipe Gomes <contato@felipegomss.com.br>
2020-07-02 18:05:31 -0300

    ler mais

--- navBar.js
+++ navBar.js
@@ -19,11 +19,11 @@ if (screen.width < 728) {
         if (pontos.style.display === "none") {
             pontos.style.display = "inline";
             maisTexto.style.display = "none";
-            btn.innerHTML = "Leia Mais";
+            btn.innerHTML = "Ler Mais";
         } else {
             pontos.style.display = "none";
             maisTexto.style.display = "inline";
-            btn.innerHTML = "Leia Menos";
+            btn.innerHTML = "Ler Menos";
         }
     }
 
@@ -35,11 +35,11 @@ if (screen.width < 728) {
         if (pontos.style.display === "none") {
             pontos.style.display = "inline";
             maisTexto.style.display = "none";
-            btn.innerHTML = "Leia Mais";
+            btn.innerHTML = "Ler Mais";
         } else {
             pontos.style.display = "none";
             maisTexto.style.display = "inline";
-            btn.innerHTML = "Leia Menos";
+            btn.innerHTML = "Ler Menos";
         }
     }
 }
